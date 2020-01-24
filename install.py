9a087b783185a702ffebdf2db0e621125c506222
Khemchan Seenanan <khemchanseenanan@gmail.com>
2020-01-23 23:08:59 -0400

    Updated the install script

--- install.py
+++ install.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import os 
+import platform
+plt = platform.system()
 
 def install():
     try:
@@ -10,7 +12,10 @@ def install():
     else:
         print(f" \033[2;33;40m ---- RUN THE FOLLOWING COMMAND!! ---- \n")
         print(" \033[1;32;40m createflaskapp command installed SUCCESSFULLY!!")
-        print(" \033[1;33;40m RUN \033[1;36;40m echo 'export PATH=$PATH\":$HOME/bin_dir\"' >> ~/.bash_profile && source ~/.bash_profile \033[1;33;40m TO complete the installation\n")
+        if(plt == "Darwin"):
+            print(" \033[1;33;40m RUN \033[1;36;40m echo 'export PATH=$PATH\":$HOME/bin_dir\"' >> ~/.bash_profile && source ~/.bash_profile \033[1;33;40m TO complete the installation\n")
+        elif(plt == "Linux"):
+            print(" \033[1;33;40m RUN \033[1;36;40m echo 'export PATH=$PATH\":$HOME/bin_dir\"' >> ~/.bashrc && source ~/.bashrc \033[1;33;40m TO complete the installation\n")
         
     
 if __name__ == "__main__":
